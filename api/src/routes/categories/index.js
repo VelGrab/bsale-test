cf1cea0dbb11f80225d68687acfba7e402a7a92e
Velgrab <velgrab@gmail.com>
2022-10-16 11:37:53 -0300

    Add documentation for routes

--- api/src/routes/categories/index.js
+++ api/src/routes/categories/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getAllCategories, getProductsByCategory } = require("../../controllers/category_controller");
+const {
+  getAllCategories,
+  getProductsByCategory,
+} = require("../../controllers/category_controller");
 
+/* This is a route handler. It is a function that is called when a request is made to the specified
+route. */
 router.get("/", async (req, res) => {
   try {
     const response = await getAllCategories();
@@ -11,14 +16,15 @@ router.get("/", async (req, res) => {
   }
 });
 
+/* This is a route handler. It is a function that is called when a request is made to the specified
+route. */
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
     const response = await getProductsByCategory(id);
     res.status(200).json(response);
-  }
-  catch (error) {
+  } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 });
--- api/src/routes/products/index.js
+++ api/src/routes/products/index.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const { getAllProducts, getProductsByNames } = require('../../controllers/product_controller')
+const {
+  getAllProducts,
+  getProductsByNames,
+} = require("../../controllers/product_controller");
 
 /* This is a route handler. It is a function that is called when a request is made to the route. */
-router.get('/', async (req, res) => {
-    try {
-        const response = await getAllProducts();
-        res.status(200).json(response)
-    } catch (error) {
-        return res.status(500).json({error: error.message})
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const response = await getAllProducts();
+    res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
-router.get('/:name', async (req, res) => {
-    const { name } = req.params;
+/* This is a route handler. It is a function that is called when a request is made to the route. */
+router.get("/:name", async (req, res) => {
+  const { name } = req.params;
 
-    try {
-        const response = await getProductsByNames(name);
-        res.status(200).json(response)
-    }
-    catch (error) {
-        return res.status(500).json({error: error.message})
-    }
-})
-
+  try {
+    const response = await getProductsByNames(name);
+    res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;

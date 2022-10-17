34f81d6b6b760f4a99bfb04dddd8dc22915d1ec8
Velgrab <velgrab@gmail.com>
2022-10-16 23:29:45 -0300

    Update Documentation for order JS

--- client/js/order.js
+++ client/js/order.js
@@ -1,6 +1,12 @@
+/* Getting the elements with the id asc and desc. */
 let orderAsc = document.getElementById("asc");
 let orderDesc = document.getElementById("desc");
 
+/**
+ * It fetches the data from the API, then it creates a div, an image, a title, a button and a price,
+ * then it appends the image, title, price and button to the div, then it appends the div to the
+ * section, then it adds a class to the div
+ */
 function getProductByAsc() {
   fetch("https://bsale-shop-test.herokuapp.com/order/asc")
     .then((response) => response.json())
@@ -24,6 +30,11 @@ function getProductByAsc() {
     .catch((error) => console.log(error));
 }
 
+/**
+ * It fetches the data from the API, then it creates a div, an image, a title, a button and a price,
+ * then it appends the image, title, price and button to the div, then it appends the div to the
+ * section, then it adds a class to the div
+ */
 function getProductByDesc() {
   fetch("https://bsale-shop-test.herokuapp.com/order/desc")
     .then((response) => response.json())
@@ -47,10 +58,14 @@ function getProductByDesc() {
     .catch((error) => console.log(error));
 }
 
+/* Adding an event listener to the orderAsc button, so when the user clicks on it, it will call the
+getProductByAsc function. */
 orderAsc.addEventListener("click", function () {
   getProductByAsc();
 });
 
+/* Adding an event listener to the orderDesc button, so when the user clicks on it, it will call the
+getProductByDesc function. */
 orderDesc.addEventListener("click", function () {
   getProductByDesc();
 });
